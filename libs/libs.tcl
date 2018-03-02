ea5cf71ae2b74300ff6214aeacd6a4bb604d8a0d
Leandro Tozzi <ltozzi@unsam.edu.ar>
2018-03-01 23:19:53 -0300

    Educational PDK Libs

---
+++ libs/libs.tcl
@@ -0,0 +1,12 @@
+read_lib nldm_op_cond_typ.lib
+write_lib op_cond_typ -output nldm_op_cond_typ.db
+
+read_lib nldm_op_cond_fast.lib
+write_lib op_cond_fast -output nldm_op_cond_fast.db
+
+read_lib nldm_op_cond_slow.lib
+write_lib op_cond_slow -output nldm_op_cond_slow.db
+
+
+
+exit
